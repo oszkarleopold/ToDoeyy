00a4d953c2f15a50f4251e37ba2e0669cbbda758
Leopold Oszkár <oszkar@leopolds-mac-mini.local>
2019-01-27 17:38:23 +0100

    Initial Commit

---
+++ ToDoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by Leopold Oszkár on 2019. 01. 27..
+//  Copyright © 2019. Leopold Oszkár. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
